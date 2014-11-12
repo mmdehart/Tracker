215d446507ba41cf07f599ee27043965ea3b26aa
Madison DeHart <mmdb83@mail.missouri.edu>
2014-11-11 22:08:01 -0600

    added tabs but still needs work

--- TrackerTeamB/TrackerTeamB/SettingsViewController.swift
+++ TrackerTeamB/TrackerTeamB/SettingsViewController.swift
@@ -1,23 +1,19 @@
 //
-//  TodayViewController.swift
+//  SettingsViewController.swift
 //  TrackerTeamB
 //
-//  Created by Madison DeHart on 11/6/14.
+//  Created by Madison DeHart on 11/11/14.
 //  Copyright (c) 2014 Madison DeHart. All rights reserved.
 //
 
 import UIKit
 
-class TodayViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var countLabel: UILabel!
-
-    @IBOutlet weak var stepper: UIStepper!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,9 +21,6 @@ class TodayViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func stepperValueChanged(sender: UIStepper) {
-        countLabel.text = Int(sender.value).description
-    }
 
     /*
     // MARK: - Navigation
